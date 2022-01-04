454a5dca60ab307b9f773b698cb811ac36942867
Wallace Alberto <1320wallace1320@outlook.com>
2022-01-04 17:55:11 -0300

    adicionado botao de logout

--- resources/views/includes/component/header-dropdown-profile.php
+++ resources/views/includes/component/header-dropdown-profile.php
@@ -4,5 +4,10 @@
 	<a href="javascript:;" class="dropdown-item">Calendar</a>
 	<a href="javascript:;" class="dropdown-item">Setting</a>
 	<div class="dropdown-divider"></div>
-	<a href="javascript:;" class="dropdown-item">Log Out</a>
+	<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+		{{ __('Logout') }}
+	</a>
+	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+		@csrf
+	</form>
 </div>
