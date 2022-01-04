ee31a2b94283dd9646021a87461a4e2d8ec39553
Wallace Alberto <1320wallace1320@outlook.com>
2022-01-04 17:56:52 -0300

    fix button logout

--- resources/views/includes/component/header-dropdown-profile.php
+++ resources/views/includes/component/header-dropdown-profile.php
@@ -4,8 +4,8 @@
 	<a href="javascript:;" class="dropdown-item">Calendar</a>
 	<a href="javascript:;" class="dropdown-item">Setting</a>
 	<div class="dropdown-divider"></div>
-	<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-		{{ __('Logout') }}
+	<a class="dropdown-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+		Logout
 	</a>
 	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 		@csrf
